624dcc3524ec21091cd3999a281987dbb54f2a64
khuwals <sunil.khuwal@gmail.com>
2020-09-09 02:39:06 +0530

    imagecard with css

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = { spans: 0 };
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const spans = this.imageRef.current.clientHeight/10;
+    this.setState({ spans });
+  };
+
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,3 +1,4 @@
+import './ImageList.css';
 import React from "react";
 import ImageCard from "./ImageCard";
 
@@ -5,7 +6,7 @@ const ImageList = (props) => {
   const imageList = props.images.map((image) => {
     return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{imageList}</div>;
+  return <div className="image-list">{imageList}</div>;
 };
 
 export default ImageList;
